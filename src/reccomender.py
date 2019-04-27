4f82d4646093a3ae976be024f93576521f3986bd
Joseph Seiler <jseiler97@gmail.com>
2019-04-26 20:58:46 -0400

    went with dataframes instead of objects

--- src/reccomender.py
+++ src/reccomender.py
@@ -1,43 +1,42 @@
 import pandas as pd
-from actor import *
 
-def make_movie(actors,directors,genre,tags):
-	movies = {}
-	current_mov_id = 0
-	for line in actors:
-		line = line.split("\t")
-		#print(line[0],type(line[0]))
-		if line[0] not in movies.keys():
-			#print("adding movie " + line[0])
-			current_mov_id = line[0]
-			movies[line[0]] = [Actor(line[2],line[1],line[3])]
-		elif line[0] == current_mov_id:
-			actor = Actor(line[2],line[1],line[3])
-			movies[line[0]].append(actor)
-			#print("adding actor" + str(actor))
-	for movie in movies.keys():
-		print("*****************************\n")
-		print("Movie:" + movie)
-		for actor in movies[movie]:
-			print(actor)
+
+def get_dataframes():
+	mov_actor_file = open("../additional_files/movie_actors.dat", encoding = "ISO-8859-1").readlines()
+	mov_directors_file = open("../additional_files/movie_directors.dat", encoding = "ISO-8859-1").readlines()
+	mov_genres_file = open("../additional_files/movie_genres.dat", encoding = "ISO-8859-1").readlines()
+	mov_tags_file = open("../additional_files/movie_tags.dat", encoding = "ISO-8859-1").readlines()
+	tags_file = open("../additional_files/tags.dat", encoding = "ISO-8859-1").readlines()
+	test_file = open("../additional_files/test.dat", encoding = "ISO-8859-1").readlines()
+	train_file = open("../additional_files/train.dat", encoding = "ISO-8859-1").readlines()
+	user_tagged_file = open("../additional_files/user_taggedmovies.dat", encoding = "ISO-8859-1").readlines()
+
+	actor_data = pd.DataFrame(data = [x.strip("\n").split("\t") for x in mov_actor_file])
+	director_data = pd.DataFrame(data = [x.strip("\n").split("\t") for x in mov_directors_file])
+	genre_data = pd.DataFrame(data = [x.strip("\n").split("\t") for x in mov_genres_file])
+	tags_data = pd.DataFrame(data = [x.strip("\n").split("\t") for x in mov_tags_file])
+
+	actor_data.columns = actor_data.loc[0].tolist()
+	actor_data.drop([0],inplace = True)
+	
+	director_data.columns = director_data.loc[0].tolist()
+	director_data.drop([0],inplace = True)
+
+	genre_data.columns = genre_data.loc[0].tolist()
+	genre_data.drop([0],inplace = True)
+
+	tags_data.columns = tags_data.loc[0].tolist()
+	tags_data.drop([0],inplace = True)
+
+	return actor_data,director_data,genre_data,tags_data
 
 def main():
-	movie_data = pd.DataFrame()
+	actor_data,director_data,genre_data,tags_data = get_dataframes()
 
-	mov_actor_file = open("../additional_files/movie_actors.dat", encoding = "ISO-8859-1")
-	mov_directors_file = open("../additional_files/movie_directors.dat", encoding = "ISO-8859-1")
-	mov_genres_file = open("../additional_files/movie_genres.dat", encoding = "ISO-8859-1")
-	mov_tags_file = open("../additional_files/movie_tags.dat", encoding = "ISO-8859-1")
-	tags_file = open("../additional_files/tags.dat", encoding = "ISO-8859-1")
-	test_file = open("../additional_files/test.dat", encoding = "ISO-8859-1")
-	train_file = open("../additional_files/train.dat", encoding = "ISO-8859-1")
-	user_tagged_file = open("../additional_files/user_taggedmovies.dat", encoding = "ISO-8859-1")
-
-	mov_actors = mov_actor_file.readlines()
-	mov_directors = mov_directors_file.readlines()
-	mov_genres = mov_genres_file.readlines()
-	mov_tags = mov_tags_file.readlines()
-	make_movie(mov_actors,mov_directors,mov_genres,mov_tags)
+	print(actor_data.head())
+	print(director_data.head())
+	print(genre_data.head())
+	print(tags_data.head())
 
 
 
